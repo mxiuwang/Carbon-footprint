2a6631ca8ab6c081e8c54d054f1fc838a079a71f
AGLandberg <47045255+AGLandberg@users.noreply.github.com>
2020-01-19 11:45:43 -0700

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,4 +1,26 @@
-function from_gasoline(fuel_type, daily_km) {
+
+var daily_kms = $("#kms").val();
+var lamb = $("#lamb").val();
+var beef = $("#beef").val();
+var pork = $("#pork").val();
+var turkey = $("#turkey").val();
+var chicken = $("#chicken").val();
+var fish = $("#fish").val();
+var other = $("#other").val();
+var cheese = $("#cheese").val();
+var eggs = $("#eggs").val();
+var milk = $("#milk").val();
+var potatoes = $("#potatoes").val();
+var rice = $("#rice").val();
+var nuts = $("#nuts").val();
+var beans = $("#beans").val();
+var fruit = $("#fruit").val();
+var vegetables = $("#vegetables").val();
+var lentils = $("#lentils").val();
+var kWh = $("#power").val();
+
+    
+function from_gasoline(daily_km) {
     
     var co2PerMile = 404;
     
@@ -6,29 +28,27 @@ function from_gasoline(fuel_type, daily_km) {
     
     var totalCo2;
     
-    if (fuel_type == 'Gasoline') {
+    totalCO2 = daily_miles * co2PerMile;
     
-        totalCO2 = daily_miles * 404;
-    
-    }
-    
+    //multiply by 7 for 7 days a week
     return totalCo2*7;
 }
 
-function from_meat(lamb, beef, pork, turkey, chicken, fish) {
+function from_meat(lamb, beef, pork, turkey, chicken, fish, other_meat) {
     
     var meats = [lamb, beef, pork, turkey, chicken, fish, other_meat];
     
-    var CO2perGram = [27,39.2,12.1,10.9,6.9,6.1];
+    var CO2perGram = [27,39.2,12.1,10.9,6.9,6.1,20];
     
     var avgMeatServing = 28.35;
     
     var totalCO2 = 0;
     
-    for (let i = 0; i < 6; i++) {
+    var i;
+    
+    for (i = 0;i<7; i++) {
         
-        totalCO2 = totalCO2 + means[i]*CO2perGram[i]*avgMeatServing;
-        
+        totalCO2 = totalCO2 + meats[i]*CO2perGram[i]*avgMeatServing;
     }
     
     return totalCO2;
@@ -44,7 +64,7 @@ function from_meat_biproducts(cheese, eggs, milk) {
     
     var totalCO2 = 0;
     
-    for (let i=0; i < 3; i++) {
+    for (var i=0; i < 3; i++) {
         
         totalCO2 = totalCO2 + bi_products[i]*CO2perGram[i]*gramsPerServing[i];
     
@@ -63,7 +83,7 @@ function other_foods(potatoes, rice, nuts, beansAndTofu, fruit, vegetables, lent
     
     var totalCO2 = 0;
     
-    for (let i=0; i < 7; i++) {
+    for (var i=0; i < 7; i++) {
         
         totalCO2 = totalCO2 + foods[i]*CO2perGram[i]*gramsPerServing[i];
     
@@ -96,22 +116,51 @@ function calculator() {
     //display the stuff inside the functions, and the total co2
     
     //return a list containing two lists: 1) sorted by amount of co2 emissions 2)a dictionary with the source of emission and amount
-    return  ["transportation", "eating meat", "meat bi-products", "power", ""]
+    var transportation = from_gasoline(daily_kms);
+    
+    var meat = from_meat(lamb, beef, pork, turkey, chicken, fish, other);
+    
+    var bi_prod = from_meat_biproducts(cheese, eggs, milk);
+    
+    var other = other_foods(potatoes, rice, nuts, beans, fruit, vegetables, lentils);
+    
+    var power = power(kWh);
+    
+    var maximum = Math.max(transportation, meat, bi_prod, other, power);
+    
+    if (transportation == maximum) {
+        
+        return "transportation";
+    }
+    else if (maximum == meat) {
+        
+        return "meat";
+    }
+    else if (bi_prod == maximum) {
+        
+        return "meat bi-products"
+    }
+    else if (other) {
+        
+        return "";
+        
+    } else {
+        
+        return "power";
+    }
 }
 
 function display() {
     // displays iframe with google search result of the highest CO2 emission from the calculator
     let highestCO2 = calculator()[0];
     let url = `http://www.google.com/search?q=ways+to+reduce+emissions+${highestCO2}&igu=1`;
-    $("#iframeweb").attr("src", url);
-    
+    $("#iframeweb").attr("src", url);  
 }
 
-$(document).ready(function(){
-    $("#send").click(function(){
-        var car = $("#carData").val();
-        var distance = $("#distanceData").val();
-    });
-});
 
 display();
+
+
+
+
+
